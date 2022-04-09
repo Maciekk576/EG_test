f3c12526e960ff8bfbc145cb1e1d36c14546ecbd
Maciej Łopusiński <57875371+Maciekk576@users.noreply.github.com>
2022-04-09 13:01:30 +0200

    Add files via upload

--- raux.R
+++ raux.R
@@ -1,9 +1,13 @@
 library(tidyverse)
 library(tidyverse)
+##### pakiety #####
+
 library(car)
 library(foreign)
 library(gplots)
 library(plm)
+
+##### import data ####
 
 # set wd
 load("C:/Users/macie/OneDrive/Pulpit/EG_test/Spotify_long.RData")
@@ -28,8 +32,7 @@ d
 Panel <- data_long
 plotmeans(valence ~ country, main="Heterogeineity across countries", data=Panel)
 
-fixed.dum <-lm(valence ~ temperature + sky +  factor(country) + factor(time) - 1, data=Panel)
-summary(fixed.dum)
+##### fixed 1-24 ####
 
 fixed_only_country <- lm(valence ~ temperature + sky + factor(country) - 1, data=Panel)
 fixed_only_time <- lm(valence ~ temperature + sky + factor(time) - 1, data=Panel)
@@ -87,20 +90,23 @@ time_coeffs <- as.numeric(fixed_twoway$coefficients[29:51])
 time <- seq(1,23)
 plot(time, time_coeffs, type = "l")
 
-# 1-12
 # korelacje 
 # OLS
+
+###### merge danych ######
 
 load("C:/Users/macie/OneDrive/Pulpit/EG_test/macro.rdata")
 View(macro)
 
 data2 <- merge(data_long, macro)#, by.x = "country", by.y = "country")
-data2
+data2$month = data2$time + (data2$time> 12)*-12
+data2$ind_prod <- as.numeric(data2$ind_prod)
+data2$unemp <- as.numeric(data2$unemp)
+data2$ret_sales <- as.numeric(data2$ret_sales)
 # View(data2)
 write.csv(data2, file = "data2.csv")
 
-##############################
-data2$month = data2$time + (data2$time> 12)*-12
+######### 1-12 ################
 
 Panel = data2
 fixed_only_time_squares <- lm(valence ~ temperature + temperature^2 + sky + sky^2 + factor(month) - 1, data=Panel)
@@ -121,3 +127,36 @@ plot(time, time_coeffs, type = "l")
 time_coeffs <- as.numeric(fixed_twoway_squares$coefficients[28:38])
 time <- seq(1,11)
 plot(time, time_coeffs, type = "l")
+
+
+##### OLS #####
+
+ols <- lm(valence ~ sky + temperature, data = data2)
+summary(ols)
+
+#### final #####
+
+# OLS
+
+formula_basic <- valence ~ sky + temperature
+formula_rich <- valence ~ sky + temperature + unemp + ret_sales + ind_prod
+
+ols_basic <- lm(formula_basic, data2)
+ols_rich <- lm(formula_rich, data2)
+summary(ols_basic)
+summary(ols_rich)
+
+stargazer::stargazer(ols_basic, ols_rich)
+
+fixed_only_country <- lm(valence ~ temperature + sky + factor(country) - 1, data=Panel)
+fixed_only_time <- lm(valence ~ temperature + sky + factor(time) - 1, data=Panel)
+fixed_twoway <-lm(valence ~ temperature + sky +  factor(country) + factor(time) - 1, data=Panel)
+stargazer::stargazer(fixed_only_time, fixed_only_country, fixed_twoway)
+stargazer::stargazer(fixed_only_country)
+stargazer_twoway(fixed_twoway)
+
+summary(fixed_only_country)
+summary(fixed_only_time)
+summary(fixed_twoway)
+
+
